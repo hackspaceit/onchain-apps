ec3e921c578dcdcfdd5775379b338f76b6ee1aa3
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-06-24 20:11:49 +0700

    upload

---
+++ src/create/experiences.ts
@@ -0,0 +1,14 @@
+export const experiences = [
+  {
+    value: 'buy-me-coffee',
+    label: 'Buy Me A Coffee',
+  },
+  {
+    value: 'mint',
+    label: 'Mint NFT',
+  },
+  {
+    value: 'paymaster-bundler',
+    label: 'Paymaster Bundler',
+  },
+];
---
+++ src/utils/git.ts
@@ -0,0 +1,11 @@
+import { execSync } from 'child_process';
+
+export function isGitInstalled() {
+  try {
+    execSync('git --version', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
